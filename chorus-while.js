f49099d876e018e6740be92525fb1391199d44b5
Taylor <ivingstaylor@gmail.com>
2020-08-18 16:34:36 +0000

    Use a while loop to repeat a chorus line + some specific individual lines on certain numbers

---
+++ chorus-while.js
@@ -0,0 +1,14 @@
+const chorus = "Let's dance!";
+
+let repeat = 0;
+
+while (repeat < 37) {
+  console.log(chorus);
+  repeat++;
+  if (repeat === 7) {
+    console.log("THESE AREN'T THE RIGHT WORDS BUT THAT'S OKAY")
+  } else if (repeat === 27)  {
+    console.log("TAKE ME HOME 2NITE")
+  }
+}
+console.log("uNTIL I'M DONE TONIGHT~~~~~~~~~~~~~~~~~~~~~~")
